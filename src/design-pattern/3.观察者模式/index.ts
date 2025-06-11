c2a4d0b2d246906bf38858b118933bee7d74e662
Ethan Wang <ethan.wang1993@outlook.com>
2025-06-11 15:14:35 +0800

    feat: 设计模式

--- src/design-pattern/3.观察者模式/index.ts
+++ src/design-pattern/3.观察者模式/index.ts
@@ -1,56 +1,57 @@
+// 应用场景
+// 1、DOM事件
+// 2、Vue组件的更新过程(vue的生命周期)
+// 3、各种的异步回调: setTimeout\setInterval\Promise.then
+// 4、MutationObserver
+
 // 发布订阅模式是通过观察者模式衍生出来的新的模式
-// DOM事件
-// Vue组件的更新过程(vue的生命周期)
-// 各种的异步回调: setTimeout\setInterval\Promise.then
-// MutationObserver
 
 // subject 主题 1个主题
 // observer 观察者 多个观察者
 
 class Subject {
-  private state: number
-  private observer: Observer[] = []
-  
+  #state: number = 0; // 主题状态
+  #observer: Observer[] = [];
+
   // 获取当前状态
   getState(): number {
-    return this.state
+    return this.#state;
   }
 
   // 设置状态
   setState(newState: number) {
-    this.state = newState
-    this.notify()
+    this.#state = newState;
+    this.notify();
   }
 
   // 添加观察者
   attach(observer: Observer) {
-    this.observer.push(observer)
+    this.#observer.push(observer);
   }
 
   // 通知观察者状态更新
   private notify() {
-    this.observer.forEach(observer => observer.update(this.state))
+    this.#observer.forEach((observer) => observer.update(this.#state));
   }
 }
 
-
 class Observer {
-  name: string
-  constructor(name) {
-    this.name = name
+  name: string;
+  constructor(name: string) {
+    this.name = name;
   }
 
   update(state: number) {
-    console.log(`${this.name} update state is ${state};`)
+    console.log(`${this.name} update state is ${state};`);
   }
 }
 
-const sub  = new Subject()
+const sub = new Subject();
 
-const observer1 = new Observer('A')
-sub.attach(observer1)
+const observer1 = new Observer("A");
+sub.attach(observer1);
 
-const observer2 = new Observer('B')
-sub.attach(observer1)
+const observer2 = new Observer("B");
+sub.attach(observer2);
 
-sub.setState(1)
+sub.setState(1);
