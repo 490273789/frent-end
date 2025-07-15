6ca9b49c0f25bc9d97ea48c46a3676a9ece678d0
Ethan Wang <ethan.wang1993@outlook.com>
2025-07-15 13:53:11 +0800

    chore: add monorepo

--- src/algorithm/tree/practise-03.js
+++ src/algorithm/tree/practise-03.js
@@ -3,28 +3,28 @@
 const postOrder = (root) => {
   const result = [];
   const traversal = (node) => {
-    if(node !== null) {
+    if (node !== null) {
       node.children?.foreach((element) => {
         traversal(element);
-      })
-      result.push(node.value)
+      });
+      result.push(node.value);
     }
-  }
+  };
   traversal(root);
   return result;
-}
+};
 
 // 用栈来迭代
 const postOrderTraversal = (root) => {
   const result = [];
   const stack = [root];
-  
-  while(stack.length) {
+
+  while (stack.length) {
     const current = stack.pop();
     result.push(current.value);
-    for(let i = current.children.length - 1; i >= 0; i--) {
+    for (let i = current.children.length - 1; i >= 0; i--) {
       stack.push(current.children[i]);
     }
   }
   return result.reverse;
-}
+};
